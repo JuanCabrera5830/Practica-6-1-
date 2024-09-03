2ce329448e68ab26a3413350ed0c901734efcf6b
JuanCabrera5830 <juan.cabrera50@unach.mx>
2024-09-02 20:42:01 -0600

    Add files via upload

---
+++ Práctica_6_1_.py
@@ -0,0 +1,11 @@
+# Crear la lista inicial
+mi_lista = [1, 2, 3, 4, 5]
+
+# Agregar un elemento al final
+mi_lista.append(6)
+
+# Insertar un elemento en la posición 2
+mi_lista.insert(2, 10)
+
+# Imprimir la lista actualizada
+print(mi_lista)
